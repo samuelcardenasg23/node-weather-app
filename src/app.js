d0123257779079cc08a6e593798b434e10509f4d
Samuel Cardenas <samuelcardenas1706@gmail.com>
2024-11-22 10:47:40 -0500

    Implement API call to getch the weather data

--- src/app.js
+++ src/app.js
@@ -4,10 +4,25 @@ const request = require("request");
 
 const app = express();
 
+const weatherData = require("../utils/weatherData");
+
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send("You must provide an address!");
+  }
+
+  weatherData(req.query.address, (error, result) => {
+    if (error) {
+      return res.send(error);
+    }
+    res.send(result);
+  });
 });
 
 app.listen(port, () => {
---
+++ utils/weatherData.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const request = require("request");
+
+const openWeatherMap = {
+  BASE_URL: process.env.OPEN_WEATHER_BASE_URL,
+  SECRET_KEY: process.env.OPEN_WEATHER_MAP_API_KEY,
+};
+
+const weatherData = (address, callback) => {
+  const url = `${openWeatherMap.BASE_URL}${encodeURIComponent(address)}&appid=${
+    openWeatherMap.SECRET_KEY
+  }`;
+  console.log(url);
+
+  request({ url, json: true }, (error, data) => {
+    if (error) {
+      callback(true, "Unable to connect to weather service!" + error);
+    }
+    callback(false, data?.body);
+  });
+};
+
+module.exports = weatherData;
